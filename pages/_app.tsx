b05e5d20d4ef15569b71e151e53ba6c94e6756c0
ambar <ambar.lee@gmail.com>
2022-04-16 11:31:03 +0800

    Add focus-visible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import {ChakraProvider, extendTheme} from '@chakra-ui/react'
+// 给 HTML 元素加上了 [data-focus-visible-added] hook，而 `@chakra-ui/css-reset` 识别了它，这样避免到处重置 outline 样式
+import 'focus-visible'
 import type {AppProps} from 'next/app'
 import '../biz/polyfill'
 import '../styles/globals.css'
