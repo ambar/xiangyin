0d04bc3876a4512bb48b18435d3dd3b84dc977be
ambar <ambar.lee@gmail.com>
2022-04-16 10:20:04 +0800

    Add GitHub link

--- biz/BasePage.tsx
+++ biz/BasePage.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 import React from 'react'
+import * as bsi from 'react-icons/bs'
 
 const tabIndexByPathname = {
   '/': 0,
@@ -16,7 +17,7 @@ const BasePage: React.FC<{children: React.ReactNode}> = ({children}) => {
   const {pathname} = router
 
   return (
-    <ui.Container maxW="6xl" px="1">
+    <ui.Container maxW="6xl" px="0">
       <Head>
         <title>湘音 - 长沙话发音字典</title>
         <meta name="description" content="长沙话,湘语,正音,方音,注音" />
@@ -41,11 +42,12 @@ const BasePage: React.FC<{children: React.ReactNode}> = ({children}) => {
 
       <ui.Box pt="4">
         <ui.Tabs
+          px="4"
           variant="soft-rounded"
           index={tabIndexByPathname[pathname as TabKey]}
           onChange={() => {}}
         >
-          <ui.TabList>
+          <ui.TabList alignItems="center">
             <Link passHref href="/">
               <ui.Tab as="a" _hover={{boxShadow: 'none'}}>
                 注音
@@ -56,6 +58,17 @@ const BasePage: React.FC<{children: React.ReactNode}> = ({children}) => {
                 音节表
               </ui.Tab>
             </Link>
+            <ui.Box ml="auto">
+              <ui.Link
+                isExternal
+                href="https://github.com/ambar/xiangyin"
+                aria-label="GitHub 源码与反馈"
+                sx={{color: 'gray', transition: 'color .15s ease-in-out'}}
+                _hover={{color: 'inherit'}}
+              >
+                <bsi.BsGithub size={20} />
+              </ui.Link>
+            </ui.Box>
           </ui.TabList>
         </ui.Tabs>
         <ui.Box p="4">{children}</ui.Box>
