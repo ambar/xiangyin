60f912a0892baaf893f0a1a5897c5f0918dcbf5c
ambar <ambar.lee@gmail.com>
2022-04-20 23:02:14 +0800

    Add XPA

--- data/湘拼.ts
+++ data/湘拼.ts
@@ -2,117 +2,152 @@ import * as m3 from '../data/汉语方音字汇.meta'
 import * as m1 from '../data/湘音检字.meta'
 import * as m2 from '../data/长沙话音档.meta'
 
-type InitialRow = [m1.Initial | null, m2.Initial | null, m3.Initial | null]
-type FinalRow = [m1.Final | null, m2.Final | null, m3.Final | null]
+type XPAIntial = string
+type XPAFinal = string
+type InitialRow = [
+  m1.Initial | null,
+  m2.Initial | null,
+  m3.Initial | null,
+  XPAIntial
+]
+type FinalRow = [m1.Final | null, m2.Final | null, m3.Final | null, XPAFinal]
 
 export const InitialConfig: InitialRow[] = [
-  ['', '', ''],
-  ['p', 'p', 'p'],
-  ['pʰ', 'pʰ', 'pʰ'],
-  ['m', 'm', 'm'],
-  ['s', 's', 's'],
-  ['ts', 'ts', 'ts'],
-  ['tsʰ', 'tsʰ', 'tsʰ'],
-  ['t', 't', 't'],
-  ['tʰ', 'tʰ', 'tʰ'],
-  ['ȵ', 'ȵ', 'ȵ'],
-  ['ɕ', 'ɕ', 'ɕ'],
-  ['k', 'k', 'k'],
-  ['kʰ', 'kʰ', 'kʰ'],
-  ['tɕ', 'tɕ', 'tɕ'],
-  ['tɕʰ', 'tɕʰ', 'tɕʰ'],
-  ['x', 'x', 'x'],
-  ['ŋ', 'ŋ', 'ŋ'],
+  ['', '', '', ''],
+  ['p', 'p', 'p', 'b'],
+  ['pʰ', 'pʰ', 'pʰ', 'p'],
+  ['m', 'm', 'm', 'm'],
+  ['s', 's', 's', 's'],
+  ['ts', 'ts', 'ts', 'z'],
+  ['tsʰ', 'tsʰ', 'tsʰ', 'c'],
+  ['t', 't', 't', 'd'],
+  ['tʰ', 'tʰ', 'tʰ', 't'],
+  ['ȵ', 'ȵ', 'ȵ', 'gn'],
+  // sh -> x
+  ['ɕ', 'ɕ', 'ɕ', 'x'],
+  ['k', 'k', 'k', 'g'],
+  ['kʰ', 'kʰ', 'kʰ', 'k'],
+  // zh -> j
+  ['tɕ', 'tɕ', 'tɕ', 'j'],
+  // ch -> q
+  ['tɕʰ', 'tɕʰ', 'tɕʰ', 'q'],
+  ['x', 'x', 'x', 'h'],
+  ['ŋ', 'ŋ', 'ŋ', 'ng'],
   // 李
-  ['l̃', null, null],
-  [null, 'l', 'l'],
+  ['l̃', null, null, 'l'],
+  [null, 'l', 'l', 'l'],
   // 发
-  ['ɸ', null, null],
-  [null, 'f', 'f'],
+  ['ɸ', null, null, 'f'],
+  [null, 'f', 'f', 'f'],
   //  紙長
-  ['tʃ', null, null],
-  [null, 'tʂ', null],
+  ['tʃ', null, null, 'zh'],
+  [null, 'tʂ', null, 'zh'],
   // 車尺
-  ['tʃʰ', null, null],
-  [null, 'tʂʰ', null],
+  ['tʃʰ', null, null, 'ch'],
+  [null, 'tʂʰ', null, 'ch'],
   // 惹
-  ['ɹ', null, null],
-  [null, 'ʐ', null],
-  [null, null, 'z'],
+  ['ɹ', null, null, 'r'],
+  [null, 'ʐ', null, 'r'],
+  [null, null, 'z', 'r'],
   // 诗尸
-  ['ʃ', null, null],
-  [null, 'ʂ', null],
+  ['ʃ', null, null, 'sh'],
+  [null, 'ʂ', null, 'sh'],
 ]
 
 export const FinalsConfig: FinalRow[] = [
-  ['ɿ', 'ɿ', 'ɿ'],
-  ['ɑ', null, null],
-  ['i', 'i', 'i'],
-  ['u', 'u', 'u'],
-  ['y', 'y', 'y'],
-  ['iɑ', null, null],
-  ['yɑ', null, null],
-  ['uɑ', null, null],
-  ['ɘ', null, null],
-  ['ie', 'ie', 'ie'],
-  ['e', null, null],
-  ['o', 'o', 'o'],
-  ['io', 'io', 'io'],
-  ['ai', 'ai', 'ai'],
-  ['uai', 'uai', 'uai'],
-  ['yai', 'yai', 'yai'],
-  ['uɘ', null, null],
-  ['ye', 'ye', 'ye'],
-  ['yɘi', null, null],
-  ['uɘi', null, null],
-  ['ɘi', null, null],
-  ['au', 'au', 'au'],
-  ['x', null, null],
-  ['iau', 'iau', 'iau'],
-  ['əu', 'əu', 'əu'],
-  ['iəu', 'iəu', 'iəu'],
-  ['ã', null, null],
-  ['iã', null, null],
-  ['uã', null, null],
-  ['iẽ', 'iẽ', 'iẽ'],
-  ['ẽ', null, null],
-  ['yã', null, null],
-  ['yẽ', 'yẽ', 'yẽ'],
-  ['on', null, null],
-  ['əȵ̍', null, null],
-  ['iȵ̍', null, null],
-  ['yȵ̍', null, null],
-  ['uəȵ̍', null, null],
-  ['ȵ̍', null, null],
-  ['aŋ', null, null],
-  ['iaŋ', null, null],
-  ['uaŋ', null, null],
-  ['yaŋ', null, null],
-  ['oŋ', 'oŋ', null],
-  ['ioŋ', 'ioŋ', null],
-  ['m̩', 'm̩', 'm̩'],
-  ['n̩', 'n̩', 'n̩'],
-  [null, 'ʅ', null],
-  [null, 'a', 'a'],
-  [null, 'ia', 'ia'],
-  [null, 'ua', 'ua'],
-  [null, 'ya', 'ya'],
-  [null, 'ə', null],
-  [null, 'uə', null],
-  [null, 'ei', 'ei'],
-  [null, 'uei', 'uei'],
-  [null, 'yei', 'yei'],
-  [null, 'õ', 'õ'],
-  [null, 'ə̃', null],
-  [null, 'an', 'an'],
-  [null, 'ian', 'ian'],
-  [null, 'uan', 'uan'],
-  [null, 'yan', 'yan'],
-  [null, 'ən', 'ən'],
-  [null, 'in', 'in'],
-  [null, 'uən', 'uən'],
-  [null, 'yn', 'yn'],
-  [null, null, 'ɤ'],
-  [null, null, 'uɤ'],
-  [null, null, 'ɤ̃'],
+  // '' -> i
+  ['ɿ', 'ɿ', 'ɿ', 'i'],
+  [null, 'ʅ', null, 'i'],
+
+  ['ɑ', null, null, 'a'],
+  [null, 'a', 'a', 'a'],
+
+  ['i', 'i', 'i', 'i'],
+  ['u', 'u', 'u', 'u'],
+  ['y', 'y', 'y', 'y'],
+
+  ['iɑ', null, null, 'ia'],
+  [null, 'ia', 'ia', 'ia'],
+
+  ['yɑ', null, null, 'ya'],
+  [null, 'ya', 'ya', 'ya'],
+
+  ['uɑ', null, null, 'ua'],
+  [null, 'ua', 'ua', 'ua'],
+
+  ['ɘ', null, null, 'e'],
+  ['ie', 'ie', 'ie', 'ie'],
+  ['e', null, null, 'e'],
+  [null, 'ə', null, 'e'],
+  [null, null, 'ɤ', 'e'],
+
+  ['o', 'o', 'o', 'o'],
+  ['io', 'io', 'io', 'io'],
+  ['ai', 'ai', 'ai', 'ai'],
+  ['uai', 'uai', 'uai', 'uai'],
+  ['yai', 'yai', 'yai', 'yai'],
+
+  ['uɘ', null, null, 'ue'],
+  [null, 'uə', null, 'ue'],
+  [null, null, 'uɤ', 'ue'],
+
+  ['ye', 'ye', 'ye', 'ye'],
+  ['yɘi', null, null, 'yei'],
+  [null, 'yei', 'yei', 'yei'],
+
+  ['uɘi', null, null, 'uei'],
+  [null, 'uei', 'uei', 'uei'],
+
+  ['ɘi', null, null, 'ei'],
+  [null, 'ei', 'ei', 'ei'],
+
+  ['au', 'au', 'au', 'au'],
+  // ['x', null, null],
+  ['iau', 'iau', 'iau', 'iau'],
+  ['əu', 'əu', 'əu', 'ou'],
+  ['iəu', 'iəu', 'iəu', 'iou'],
+  ['ã', null, null, 'an'],
+  [null, 'an', 'an', 'an'],
+
+  ['iã', null, null, 'ian'],
+  [null, 'ian', 'ian', 'ian'],
+
+  ['uã', null, null, 'uan'],
+  [null, 'uan', 'uan', 'uan'],
+
+  ['iẽ', 'iẽ', 'iẽ', 'ienn'],
+
+  ['ẽ', null, null, 'enn'],
+  [null, 'ə̃', null, 'enn'],
+  [null, null, 'ɤ̃', 'enn'],
+
+  ['yã', null, null, 'yan'],
+  [null, 'yan', 'yan', 'yan'],
+
+  ['yẽ', 'yẽ', 'yẽ', 'yenn'],
+  ['on', null, null, 'on'],
+  [null, 'õ', 'õ', 'on'],
+
+  ['əȵ̍', null, null, 'en'],
+  [null, 'ən', 'ən', 'en'],
+
+  ['iȵ̍', null, null, 'in'],
+  [null, 'in', 'in', 'in'],
+
+  ['yȵ̍', null, null, 'yn'],
+  [null, 'yn', 'yn', 'yn'],
+
+  ['uəȵ̍', null, null, 'uen'],
+  [null, 'uən', 'uən', 'uen'],
+
+  ['ȵ̍', null, null, 'n'],
+  ['aŋ', null, null, 'ang'],
+  ['iaŋ', null, null, 'iang'],
+  ['uaŋ', null, null, 'uang'],
+  ['yaŋ', null, null, 'yang'],
+  ['oŋ', 'oŋ', null, 'ong'],
+  ['ioŋ', 'ioŋ', null, 'iong'],
+
+  ['m̩', 'm̩', 'm̩', 'm'],
+  ['n̩', 'n̩', 'n̩', 'n'],
 ]
